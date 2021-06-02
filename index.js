3a2930dd1b3f3a774b340914bf267ca0cf0849be
Agnes-CeciliaBergqvist <agnes.bergqvist@hotmail.com>
2021-06-02 18:35:02 +0200

    aa

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ app.post('/create-checkout-session', async (req, res) => {
   res.json({ id: session.id });
 });
 
-app.listen(80);
+const port = process.env.PORT || 3000
+app.listen(port);
 //localhost:4242
 //ska se om detta funkar 
